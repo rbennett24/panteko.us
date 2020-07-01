0bde23e1e1ce026a07f493191a08e9477778e92d
Robert Henderson <balamkej@gmail.com>
2020-07-01 08:48:31 -0700

    working code to format examples

--- expex_to_html/glossed_examples.py
+++ expex_to_html/glossed_examples.py
@@ -1,10 +1,45 @@
+import re
 
+gloss_div = """<div data-gloss>
+	<p>{l1}</p>
+	<p>{gls}</p>
+	<p>{l2}</p>
+</div>"""
+
+html = """<html>
+	<head>
+		<link rel="stylesheet" href="leipzig.js/dist/leipzig.css">
+	</head>
+	<body>
+		{exs}
+		<script src="leipzig.js/dist/leipzig.js"></script>
+    	<script>
+    		document.addEventListener('DOMContentLoaded', function(){{
+        		Leipzig().gloss();
+        	}});
+        </script>
+    </body>
+</html>"""
+
+def format_gls(f):
+	gloss_list = []
+	for line in f:
+		x = re.split(r'\t+', line)
+		gloss_block = gloss_div.format(l1 = x[0], gls = x[1], l2 = x[2])
+		gloss_list.append(gloss_block)
+
+	return '\n'.join(gloss_list)
 
 
 def main():
 
+	with open("xoqoneb_examples_tab.txt", "r") as txtfile:
+		s = format_gls(txtfile)
+		h = html.format(exs = s)
+
+
 	with open("examples.html", "w") as htmlfile:
-		pass
+		htmlfile.write(h)
 		
 if __name__ == '__main__':
 	main() 
